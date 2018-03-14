a50deed11b76f24901bc138d70ae206bb10cfe51
Christian Winkler <realgoldencoldlp@gmail.com>
2018-03-14 09:03:31 +0100

    fix

--- backend.js
+++ backend.js
@@ -24,27 +24,29 @@ $(document).ready(function(){
         console.log(rightOrder);
         $(".btn").off().on("click", function() {
           $("#sound-" + $(this).attr("id"))[0].play();
-          userOrder.push($(this).attr("id"));
-          for (var i = 0; i < userOrder.length; i++) {
-            if (JSON.stringify(rightOrder) === JSON.stringify(userOrder)) {
-              userOrder = [];
-              nextRound();
-              startGame();
-              break;
-            }
-            if (rightOrder[i] !== userOrder[i]) {
-              if (strict === false && lastChance === true) {
-                lastChance = false;
-                alert("Don't fail again ;)");
+          setTimeout(function(){
+            userOrder.push($(this).attr("id"));
+            for (var i = 0; i < userOrder.length; i++) {
+              if (JSON.stringify(rightOrder) === JSON.stringify(userOrder)) {
                 userOrder = [];
-                animateOrderButton(rightOrder);
-              } else if (lastChance === false) {
-                $("#sound-fail")[0].play();
-                alert("You lost :(");
-                resetGame();
+                nextRound();
+                startGame();
                 break;
               }
-            }
+              if (rightOrder[i] !== userOrder[i]) {
+                if (strict === false && lastChance === true) {
+                  lastChance = false;
+                  alert("Don't fail again ;)");
+                  userOrder = [];
+                  animateOrderButton(rightOrder);
+                } else if (lastChance === false) {
+                  $("#sound-fail")[0].play();
+                  setTimeout(function(){alert("You lost :(");}, 500);
+                  resetGame();
+                  break;
+                }
+              }
+            }, 500);
           }
         });
       }
